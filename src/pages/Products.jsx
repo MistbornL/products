5c369c161ef4f4f45700c125ac36a7dda33579cc
MistbornL <lashaloria3@gmail.com>
2022-12-01 17:11:20 +0400

    filter

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Menu } from "../components/Menu";
 import { Data } from "../data/DummyData";
 
 export const Products = () => {
   const navigate = useNavigate();
-  // localStorage.setItem("products", JSON.stringify(Data));
-  const products = JSON.parse(localStorage.getItem("products"));
-  console.log(products);
+  const [isFilter, setIsFilter] = useState(false);
+  const [table, setTable] = useState(Data);
+  const name = useRef();
+  const priceFrom = useRef();
+  const priceTo = useRef();
+
+  const filterLogic = () => {
+    if (
+      name.current.value &&
+      priceFrom.current.value === "" &&
+      priceTo.current.value === ""
+    ) {
+      setTable(
+        table.filter((data) => {
+          return data.name
+            .toLowerCase()
+            .includes(name.current.value.toLowerCase());
+        })
+      );
+    } else if (name.current.value && priceFrom.current.value >= 1000) {
+      setTable(
+        table.filter((data) => {
+          return (
+            data.name
+              .toLowerCase()
+              .includes(name.current.value.toLowerCase()) &&
+            data.listPrice >= 1000
+          );
+        })
+      );
+    } else if (name.current.value && priceFrom.current.value <= 1000) {
+      setTable(
+        table.filter((data) => {
+          return (
+            data.name
+              .toLowerCase()
+              .includes(name.current.value.toLowerCase()) &&
+            data.listPrice <= 1000
+          );
+        })
+      );
+    } else if (priceFrom.current.value >= 1000) {
+      setTable(
+        table.filter((data) => {
+          return data.listPrice >= 1000;
+        })
+      );
+    } else if (priceFrom.current.value <= 1000) {
+      setTable(
+        table.filter((data) => {
+          return data.listPrice <= 1000;
+        })
+      );
+    }
+  };
   return (
     <div className="App">
       <header>
@@ -16,9 +68,35 @@ export const Products = () => {
       <main className="main">
         <div className="mb-2 d-flex justify-content-between">
           <h1>Products</h1>
-          <button className="btn btn-primary">show filter</button>
+          <button
+            onClick={() => setIsFilter(!isFilter)}
+            style={{ background: "white", color: "grey" }}
+            className="btn btn-primary"
+          >
+            show filter
+          </button>
         </div>
-
+        {isFilter ? (
+          <div className="d-flex gap-1">
+            <p>Name:</p>
+            <input
+              ref={name}
+              style={{ width: "150px" }}
+              className="form-control h-25 "
+            />
+            <p>and price between:</p>
+            <input ref={priceFrom} className="form-control h-50 w-25" />
+            <p>and</p>
+            <input ref={priceTo} className="form-control h-50 w-25" />
+            <button
+              onClick={filterLogic}
+              style={{ background: "white", color: "grey" }}
+              className="btn btn-primary"
+            >
+              Apply
+            </button>
+          </div>
+        ) : null}
         <table className="table table-striped">
           <thead>
             <tr>
@@ -31,7 +109,7 @@ export const Products = () => {
             </tr>
           </thead>
           <tbody>
-            {products.map((product) => {
+            {table.map((product) => {
               return (
                 <tr key={product.id}>
                   <th scope="row">{product.name}</th>
